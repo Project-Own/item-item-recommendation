c92da2efa7d9ddfc43075fed3fefb6093ada7696
Sahas Prajapati <b10prajapati@gmail.com>
2020-11-30 16:18:06 +0545

    Fix

--- Procfile
+++ Procfile
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-web:python flask_app.py
-=======
 web: gunicorn app:app
->>>>>>> 6b8f47212f2421f9a5ccc44af5f50d5ea0693b62
--- app.py
+++ app.py
@@ -58,8 +58,7 @@ def movie_recommend():
 
 
 if __name__=='__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run()
 
 
 
